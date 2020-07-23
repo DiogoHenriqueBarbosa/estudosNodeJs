ce7aeb9d80e03784aeb2ed75baa7e201ca29880a
Diogo <contato.diogohbarbosa@outlook.com>
2020-07-22 21:21:32 -0300

    ok

--- routes/admin.js
+++ routes/admin.js
@@ -19,16 +19,36 @@ router.get('/categoria/add', (req, res) => {
 })
 
 router.post('/categorias/nova', (req, res) => {
+    var erros = []
+    
     const novaCategoria = {
         nome: req.body.nome,
         slug: req.body.slug
     }
 
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log("Sucesso")
-    }).catch((err)=>{
-        console.log(`Erro ${err}`)
-    })
+    if(!novaCategoria.nome || typeof novaCategoria.nome == undefined || novaCategoria.nome == null){
+        erros.push({texto:"Nome invalido"})
+    }
+    if(!novaCategoria.slug || typeof novaCategoria.slug == undefined || novaCategoria.slug == null){
+        erros.push({texto: "Slug invalido"})
+    }
+    if(req.body.nome.length <= 2 ){
+        erros.push({texto: "Tamanho de nome invalido"})
+    } 
+
+    if(erros.length > 0){
+        res.render('admin/addcategorias', {erros:erros})
+    }else{
+        new Categoria(novaCategoria).save().then(()=>{
+            req.flash('successMsg', )
+            res.redirect('/admin/categoria')
+            console.log("Sucesso")
+        }).catch((err)=>{
+            console.log(`Erro ${err}`)
+        })
+    }
+        
+    
 })
 
 export default router
